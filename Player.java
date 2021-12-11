d7ff630de228b81a4331f2608f9640a34c6c684f
sujin <12194076@inha.edu>
2021-12-11 15:57:03 +0900

    기말고사 연습중(2)

---
+++ Player.java
@@ -0,0 +1,24 @@
+// 학번 : 12194076
+// 성명 : 이수진
+
+package mid;
+
+interface Prime{
+
+    void haha
+}
+class Game {
+
+
+}
+class Player{}
+
+public class lsj12194076{
+    public static void main(String[] args) {
+        Game g = new Game;
+
+
+        System.out.println();
+    }
+
+}
